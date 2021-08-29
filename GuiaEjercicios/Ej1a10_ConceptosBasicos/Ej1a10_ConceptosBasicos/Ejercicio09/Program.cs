a5f8a0659bb2c631ab3dac11b0b630249185f120
Silvia-Rus <silviarus.biblio@gmail.com>
2021-08-29 15:00:32 -0300

    Faltan el 5 y el 7.

---
+++ GuiaEjercicios/Ej1a10_ConceptosBasicos/Ej1a10_ConceptosBasicos/Ejercicio09/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Escribir un programa que imprima por pantalla una pirámide como la siguiente:
+            //*
+            //***
+            //*****
+            //*******
+            //*********
+            //El usuario indicará cuál será la altura de la pirámide ingresando un número entero positivo. 
+            //Para el ejemplo anterior la altura ingresada fue de 5.
+
+            int tamanioPiramide = 0;
+            string saltoDeLinea = "\n";
+            string caracterDibujado = "*";
+            int numeroCaracateresADibujar = 1;
+
+            Console.Title = "Ejercicio 09";
+         
+            do
+            {
+                Console.WriteLine("Ingrese un tamaño para la pirámide: ");
+
+                if(int.TryParse(Console.ReadLine(), out tamanioPiramide) && tamanioPiramide > 0)
+                {
+                    //repetir las lineas
+                    for(int i = 0; i<tamanioPiramide; i++)
+                    {
+                        //repetir las estrellas
+                        for (int j = 0; j < numeroCaracateresADibujar; j++)
+                        {
+                            Console.Write(caracterDibujado);
+                            
+                        }
+                        numeroCaracateresADibujar += 2;
+                        Console.Write(saltoDeLinea);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("ERROR. Debe ser un número entero positivo.");
+                }
+
+            } while (tamanioPiramide < 1);
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ GuiaEjercicios/Ej1a10_ConceptosBasicos/Ej1a10_ConceptosBasicos/Ejercicio10/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //10.Partiendo de la base del ejercicio anterior, se pide realizar un programa que imprima por pantalla una pirámide como la siguiente:
+            //                                                                *
+            //                                                               ***
+            //                                                              *****
+            //                                                             *******
+            //                                                            *********
+            //Nota: Utilizar estructuras repetitivas y selectivas.
+
+            int tamanioPiramide = 0;
+            string saltoDeLinea = "\n";
+            string caracterDibujado = "*";
+            string espacio = " ";
+            int numeroCaracateresADibujar = 1;
+            int numeroEspaciosADibujar;
+
+            Console.Title = "Ejercicio 10";
+
+            do
+            {
+                Console.WriteLine("Ingrese un tamaño para la pirámide: ");
+
+                if (int.TryParse(Console.ReadLine(), out tamanioPiramide) && tamanioPiramide > 0)
+                {
+                    //repetir las lineas
+                    numeroEspaciosADibujar = tamanioPiramide;
+                    for (int i = 0; i < tamanioPiramide; i++)
+                    {
+                        //repetir los espacios
+                        for(int k = numeroEspaciosADibujar;k>=0; k--)
+                        {
+                            Console.Write(espacio);
+                        }
+                                              
+                        //repetir las estrellas
+                        for (int j = 0; j < numeroCaracateresADibujar; j++)
+                        {
+                            Console.Write(caracterDibujado);
+
+                        }
+                        numeroCaracateresADibujar += 2;
+                        numeroEspaciosADibujar -= 1;
+
+                        Console.Write(saltoDeLinea);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("ERROR. Debe ser un número entero positivo.");
+                }
+
+            } while (tamanioPiramide < 1);
+
+            Console.ReadLine();
+        }
+    }
+}
